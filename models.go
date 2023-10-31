a112bf2d28b765db6880426e1457dd56eec7f272
nronzel <86695181+nronzel@users.noreply.github.com>
2023-10-31 14:40:11 -0400

    feat: types for Post and post conversion

--- models.go
+++ models.go
@@ -95,3 +95,34 @@ func nullTimeToTimePtr(t sql.NullTime) *time.Time {
 	}
 	return nil
 }
+
+func nullStringToStringPtr(s sql.NullString) *string {
+	if s.Valid {
+		return &s.String
+	}
+	return nil
+}
+
+type Post struct {
+	ID          uuid.UUID  `json:"id"`
+	CreatedAt   time.Time  `json:"created_at"`
+	UpdatedAt   time.Time  `json:"updated_at"`
+	Title       string     `json:"title"`
+	Url         string     `json:"url"`
+	Description *string    `json:"description"`
+	PublishedAt *time.Time `json:"published_at"`
+	FeedID      uuid.UUID  `json:"feed_id"`
+}
+
+func databasePostToPost(post database.Post) Post {
+	return Post{
+		ID:          post.ID,
+		CreatedAt:   post.CreatedAt,
+		UpdatedAt:   post.UpdatedAt,
+		Title:       post.Title,
+		Url:         post.Url,
+		Description: nullStringToStringPtr(post.Description),
+		PublishedAt: nullTimeToTimePtr(post.PublishedAt),
+		FeedID:      post.FeedID,
+	}
+}
